c00a682cebc2516c101e8f006f3f07c83231da26
SOMI YOON <omsi6@sookmyung.ac.kr>
2026-09-15 15:56:42 +0900

    03_exercise_3.c

--- 03_3/03_exercise_3.c
+++ 03_3/03_exercise_3.c
@@ -7,7 +7,6 @@ int main(void)
     printf("Enter a character : ");
     scanf("%c", &c);
 
-    printf("The next character of %c (%d) is %c (%d)\n", c, c, c + 1, c + 1);
     printf("The next character of %c (%i) is %c (%i)\n", c, c, c + 1, c + 1);
 
     return 0;
